a867b048a865ad666f16fa6f64664e8d35e3d2ae
Marcel Badua <bitterdash@gmail.com>
2026-02-09 07:37:59 +0800

    for 2026

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,11 +1,5 @@
-jQuery.noConflict();
-(function( $ ) {
-
-$('*[data-toggle="activate"]').on('click', function(event) {
-    event.preventDefault();
-    var target = $(this).data('target');
-    $(this).toggleClass('is-active');
-    $(target).toggleClass('is-active');
-});
-
-})(jQuery);
+document.querySelector('.nav-toggle')
+    .addEventListener('click', () => {
+        document.getElementById('site-header')
+            .classList.toggle('nav-open');
+    });
